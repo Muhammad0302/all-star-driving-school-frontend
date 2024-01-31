9450e9d494f5c0e3827a912400532aa408b77ff1
@mi477048 <mi477048@gmail.com>
2024-01-31 17:51:58 -0300

    update: changes added

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,61 +1,13 @@
 import React, { useState } from 'react'
-import MenuIcon from '@mui/icons-material/Menu'
-import {
-  AppBar,
-  Box,
-  Menu,
-  MenuItem,
-  styled,
-  Grid,
-  Toolbar,
-  Typography,
-  Avatar,
-  FormControl,
-  InputLabel,
-  Select,
-  Modal,
-  Popover,
-  List,
-  ListItem,
-} from '@mui/material'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation'
+
+import { AppBar, styled, Toolbar, Typography } from '@mui/material'
+import Profile from './Profile'
 const Navbar = () => {
-  const [open, SetOpen] = useState(false)
   // const router = usePathname()
-  const router = useRouter()
   const StyledToolbar = styled(Toolbar)({
-    // display: "flex",
     justifyContent: 'space-between',
     height: '88px',
   })
-  const SocialBox = styled(Box)({
-    display: 'flex',
-    // gap: 12,
-  })
-  const SearchBox = styled(Box)({
-    display: 'flex',
-    gap: 5,
-  })
-  const handleLogin = () => {
-    router.push('/signin')
-  }
-
-  const [modalOpen, setModalOpen] = useState(false)
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
-
-  const handleAvatarClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget)
-    setModalOpen(true)
-  }
-
-  const handleClose = () => {
-    setModalOpen(false)
-    setAnchorEl(null)
-  }
-
-  const avatarRect = anchorEl?.getBoundingClientRect()
 
   return (
     <AppBar
@@ -80,30 +32,7 @@ const Navbar = () => {
 
         <Typography />
 
-        <Avatar src='/broken-image.jpg' onClick={handleAvatarClick} style={{ cursor: 'pointer' }} />
-
-        <Modal
-          open={modalOpen}
-          onClose={handleClose}
-          aria-labelledby='simple-modal-title'
-          aria-describedby='simple-modal-description'
-          style={{
-            position: 'absolute',
-            top: avatarRect?.top || 0,
-            left: avatarRect?.left || 0,
-          }}
-        >
-          <div
-            style={{ transform: 'translate(-10%, 50%)' }}
-            className=' bg-white border-2 rounded-lg w-[9.5rem] px-4 p-1 text-black'
-          >
-            <h3 className=' text-base'>Options</h3>
-
-            <p className=' text-sm cursor-pointer' onClick={handleLogin}>
-              Login
-            </p>
-          </div>
-        </Modal>
+        <Profile />
       </StyledToolbar>
     </AppBar>
   )
