46be7778abde705e6a37ed2f845b1d5a11c0f7d2
@mi477048 <mi477048@gmail.com>
2024-02-01 11:02:31 -0300

    update: signin page updating

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -5,10 +5,10 @@ import Signin from '@/components/Signin'
 export default function SignIn() {
   return (
     <>
-      <div className='flex'>
-        <div className='hidden sm:hidden md:hidden lg:block lg:w-[50%] xl:w-[50%]'>
+      <div className='flex h-full '>
+        <div className='hidden sm:hidden md:hidden lg:block lg:w-[50%] xl:w-[50%] lg:h-full xl:h-full'>
           <img
-            className='lg:h-[100vh] lg:w-[100%] xl:h-[100%] xl:w-[100%] xll:w-[100%]'
+            className='lg:h-[100vh] lg:w-[100%] xl:h-[100vh] xl:w-[100%] '
             style={{ objectFit: 'cover' }}
             src='Images/sign_in_pic.jpg'
             alt='library-img'
--- components/Signin/Signin.tsx
+++ components/Signin/Signin.tsx
@@ -160,7 +160,7 @@ export default function Signin() {
                     },
                     '& .MuiOutlinedInput-root': {
                       '& > fieldset': {
-                        borderColor: '#FF5B39',
+                        borderColor: '#f23d4d !important',
                       },
                     },
                     width: '57%',
@@ -184,7 +184,7 @@ export default function Signin() {
                         >
                           <path
                             d='M21.25 0H3.75C2.75544 0 1.80161 0.395088 1.09835 1.09835C0.395088 1.80161 0 2.75544 0 3.75V16.25C0 17.2446 0.395088 18.1984 1.09835 18.9017C1.80161 19.6049 2.75544 20 3.75 20H21.25C22.2446 20 23.1984 19.6049 23.9017 18.9017C24.6049 18.1984 25 17.2446 25 16.25V3.75C25 2.75544 24.6049 1.80161 23.9017 1.09835C23.1984 0.395088 22.2446 0 21.25 0ZM20.4125 2.5L12.5 8.4375L4.5875 2.5H20.4125ZM21.25 17.5H3.75C3.41848 17.5 3.10054 17.3683 2.86612 17.1339C2.6317 16.8995 2.5 16.5815 2.5 16.25V4.0625L11.75 11C11.9664 11.1623 12.2295 11.25 12.5 11.25C12.7705 11.25 13.0336 11.1623 13.25 11L22.5 4.0625V16.25C22.5 16.5815 22.3683 16.8995 22.1339 17.1339C21.8995 17.3683 21.5815 17.5 21.25 17.5Z'
-                            fill='white'
+                            fill='#f23d4d'
                           />
                         </svg>
                       </InputAdornment>
@@ -207,10 +207,10 @@ export default function Signin() {
                     },
                     '& .MuiOutlinedInput-root': {
                       '& > fieldset': {
-                        borderColor: '#FF5B39',
+                        borderColor: '#f23d4d !important',
                       },
                       '& > placeholder': {
-                        borderColor: '#FF5B39',
+                        borderColor: '#f23d4d !important',
                       },
                     },
                     width: '57%',
@@ -232,7 +232,7 @@ export default function Signin() {
                         >
                           <path
                             d='M2.875 30C2.08437 30 1.40779 29.7205 0.84525 29.1614C0.28175 28.6014 0 27.9286 0 27.1429V12.8571C0 12.0714 0.28175 11.3986 0.84525 10.8386C1.40779 10.2795 2.08437 10 2.875 10H4.3125V7.14286C4.3125 5.16667 5.01352 3.4819 6.41556 2.08857C7.81665 0.69619 9.51146 0 11.5 0C13.4885 0 15.1838 0.69619 16.5859 2.08857C17.987 3.4819 18.6875 5.16667 18.6875 7.14286V10H20.125C20.9156 10 21.5927 10.2795 22.1562 10.8386C22.7187 11.3986 23 12.0714 23 12.8571V27.1429C23 27.9286 22.7187 28.6014 22.1562 29.1614C21.5927 29.7205 20.9156 30 20.125 30H2.875ZM11.5 22.8571C12.2906 22.8571 12.9677 22.5776 13.5312 22.0186C14.0937 21.4586 14.375 20.7857 14.375 20C14.375 19.2143 14.0937 18.5414 13.5312 17.9814C12.9677 17.4224 12.2906 17.1429 11.5 17.1429C10.7094 17.1429 10.0328 17.4224 9.47025 17.9814C8.90675 18.5414 8.625 19.2143 8.625 20C8.625 20.7857 8.90675 21.4586 9.47025 22.0186C10.0328 22.5776 10.7094 22.8571 11.5 22.8571ZM7.1875 10H15.8125V7.14286C15.8125 5.95238 15.3932 4.94048 14.5547 4.10714C13.7161 3.27381 12.6979 2.85714 11.5 2.85714C10.3021 2.85714 9.28385 3.27381 8.44531 4.10714C7.60677 4.94048 7.1875 5.95238 7.1875 7.14286V10Z'
-                            fill='white'
+                            fill='#f23d4d'
                           />
                         </svg>
                       </InputAdornment>
@@ -250,7 +250,7 @@ export default function Signin() {
                   textTransform: 'capitalize',
                   fontWeight: '500',
                   fontSize: '18px',
-                  color: '#FF5B39',
+                  color: '#f23d4d',
                   lineHeight: '34px',
                   marginLeft: {
                     xs: '193px',
@@ -276,14 +276,17 @@ export default function Signin() {
                 <Button
                   type='submit'
                   sx={{
-                    color: 'white',
-                    background: 'linear-gradient(93.04deg, #FF4B2A 3.62%, #FF7551 101.83%)',
+                    color: 'black',
+                    background: '#f23d4d',
                     borderRadius: '12px',
                     padding: '14px 56px',
                     fontFamily: 'Russo One',
                     fontWeight: '400',
                     fontSize: '18px',
                     textTransform: 'none',
+                    '&:hover': {
+                      background: '#e01527',
+                    },
                   }}
                   onClick={handleLogin}
                 >
@@ -307,7 +310,7 @@ export default function Signin() {
                     <Link
                       href='/signup'
                       style={{
-                        color: '#FF5B39',
+                        color: '#f23d4d',
                         textDecoration: 'none',
                         fontFamily: 'Roboto',
                         fontWeight: '500',
