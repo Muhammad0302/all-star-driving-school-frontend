4880bda6502f35351cff03f6d28737ff91ebac4c
@mi477048 <mi477048@gmail.com>
2024-02-05 10:22:36 -0300

    update: changes added

--- app/head.tsx
+++ app/head.tsx
@@ -4,7 +4,7 @@ export default function Head() {
       <title>All Star Driving School Ltd</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <meta name='description' content='All Star Driving School Ltd' />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='/favicon.jpg' />
       {/* <link rel="preconnect" href="https://fonts.googleapis.com"/>
 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
 <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet"/> */}
