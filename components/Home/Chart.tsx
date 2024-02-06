eb8f565c5692b20751555c72d9fd232ca513252f
@mi477048 <mi477048@gmail.com>
2024-02-06 16:10:07 -0300

    update: changes added

--- components/Home/Chart.tsx
+++ components/Home/Chart.tsx
@@ -13,10 +13,10 @@ const Chart = ({ title, data, dataKey, grid }: ChartInput) => {
       <h3 className='chartTitle'>{title}</h3>
       <ResponsiveContainer width='100%' aspect={4 / 1}>
         <LineChart data={data}>
-          <XAxis dataKey='name' stroke='#5550bd' />
-          <Line type='monotone' dataKey={dataKey} stroke='#5550bd' />
+          <XAxis dataKey='name' stroke='#f23d4d' />
+          <Line type='monotone' dataKey={dataKey} stroke='#f23d4d' />
           <Tooltip />
-          {grid && <CartesianGrid stroke='#e0dfdf' strokeDasharray='5 5' />}
+          {grid && <CartesianGrid stroke='#b5b3b3' strokeDasharray='5 5' />}
         </LineChart>
       </ResponsiveContainer>
     </div>
